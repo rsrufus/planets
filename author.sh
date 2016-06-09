1471810c43e97687cfb200b4c91cd37b697c8dab
Rahmira Rufus <rsrufus@aggies.ncat.edu>
2016-06-09 14:33:20 +0000

    Commit from Rahmira

--- author.sh
+++ author.sh
@@ -1,6 +1,15 @@
+Comments:
+Create script called author.sh, which will loop over all input arguments,
+and for each input argument (the name of a file),
+it will print the name of the file, and then the line with the name of the author.
+
 for name in *.pdb;
 do
       echo $name
         head -n2 $name | tail -n1
         echo
 done
+
+More Comments:
+Today 06/09/16 we are learning how to compare git versions with the diff
+command.
